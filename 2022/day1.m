a50d65be56eda4730f1fccb5970fcfe89396229f
Manuel Schubert <manuel.schubert@tu-braunschweig.de>
2025-01-10 17:47:06 +0100

    Day 1 2022 finished.

--- 2022/day1.m
+++ 2022/day1.m
@@ -1,40 +1,48 @@
 clc; clearvars;
 % Advent of code 2022 - day 1 - part 1+2
 % Open file and take needed data
-file_id = fopen("day1_test.dat");
-data = textscan(file_id,strcat('%f %f'),'delimiter',', ','MultipleDelimsAsOne',1);
+file_id = fopen("day1.dat");
+data = textscan(file_id,strcat('%f'));
 % Close file
 fclose(file_id);
 
-left = data{1,1}; right = data{1,2};
+calories = data{:,:};
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Part 1 - Pair up left and right with increasing number and calculate
 % differences between. Add the distances up for the result
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 tic
-left_ordered = sort(left);
-right_ordered = sort(right);
-% Get distances
-dist = abs(left_ordered-right_ordered);
-result1 = sum(dist);
+% Go through elves
+idx = 1; sum = 0;
+for i = 1:length(calories(:,1))
+    if (calories(i,1)==0)
+        cal_sum(idx) = sum;
+        idx = idx+1;
+        sum = 0;
+    else
+        sum = sum+calories(i,1);
+    end
+end
+cal_sum(idx) = sum;
+% Get
+
+result1 = max(cal_sum);
 
 fprintf('%10f',result1)
 fprintf('\n')
 toc
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Part 2 - Determine similarity scores between the lists
+% Part 2 - Same but we need to add up the three top caloric elves
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 tic 
-sim_score = 0;
-for i=1:size(left)
-    % Find how often number at current index appears in right
-    cnt = sum(right == left(i));
-    sim_score = sim_score + left(i)*cnt;
-end
+one = max(cal_sum);
+two = max(cal_sum(cal_sum<one));
+three = max(cal_sum(cal_sum<two));
+result2 = one+two+three;
 
-fprintf('%10f',sim_score)
+fprintf('%10f',result2)
 fprintf('\n')
 toc
 
